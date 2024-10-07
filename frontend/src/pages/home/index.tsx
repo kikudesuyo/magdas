c6cded1f18cd5ce1ad80ea6f169fa6a7256fe386
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2024-10-07 23:11:32 +0900

    homeページ修正

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,23 +1,29 @@
 import Main from "@/components/Main";
 import { Link } from "react-router-dom";
 import { PATHS } from "@/utils/constant";
+import References from "@/pages/home/_components/References";
+import MagdasMap from "@/assets/imgs/MAGDAS_map.png";
 
 const Home = () => {
   return (
-    <Main style="items-center gap-8 pt-4">
-      <div className="flex flex-row gap-12">
-        <Link
-          to={PATHS.EE_INDEX}
-          className="text-2xl bg-slate-200 px-4 py-3 hover:bg-slate-400 round-xl"
-        >
-          EE-indexのプロットページ
-        </Link>
-        <Link
-          to={PATHS.DOWNLOAD}
-          className="text-2xl bg-slate-200 px-4 py-3 hover:bg-slate-400 rounded-lg"
-        >
-          ダウンロードページ
-        </Link>
+    <Main style="items-center gap-12 pt-4">
+      <div className="flex flex-col gap-12 items-center justify-center">
+        <div className="flex flex-row gap-12 items-center justify-center">
+          <Link
+            to={PATHS.EE_INDEX}
+            className="text-3xl bg-slate-200 px-4 py-3 hover:bg-slate-400 round-xl"
+          >
+            EE-index Plot
+          </Link>
+          <Link
+            to={PATHS.DOWNLOAD}
+            className="text-3xl bg-slate-200 px-4 py-3 hover:bg-slate-400 rounded-lg"
+          >
+            Download
+          </Link>
+        </div>
+        <img src={MagdasMap} alt="" className="max-w-4xl" />
+        <References />
       </div>
     </Main>
   );
