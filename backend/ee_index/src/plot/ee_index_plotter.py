760888b2f971a073576cd9d19b3655488da8792c
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2025-01-25 16:45:45 +0900

    rename

--- backend/ee_index/src/plot/ee_index_plotter.py
+++ backend/ee_index/src/plot/ee_index_plotter.py
@@ -7,12 +7,12 @@ from ee_index.src.calc.euel_index import Euel
 from ee_index.src.plot.base_ee_index_plotter import BaseEeIndexPlotter
 
 
-class MultiDayEeIndexPlotter:
+class EeIndexPlotter:
     """Plotter for daily EE index"""
 
-    def __init__(self, start_datetime: datetime, end_datetime: datetime):
-        self.start_datetime = start_datetime
-        self.days = (end_datetime - start_datetime).days + 1
+    def __init__(self, start_date: datetime, end_date: datetime):
+        self.start_datetime = start_date
+        self.days = (end_date - start_date).days + 1
         self.base_plotter = BaseEeIndexPlotter()
 
     def calc_ee_values(self, station):
@@ -24,19 +24,19 @@ class MultiDayEeIndexPlotter:
     def save_er_figure(self, station, save_path):
         er = Er(station, self.start_datetime).calc_er_for_days(self.days)
         self.base_plotter.plot_er(er)
-        self.base_plotter.customize_er_plot(station, self.start_datetime, len(er))
+        self.base_plotter.customize_er(station, self.start_datetime, len(er))
         self.base_plotter.save_figure(save_path)
 
     def save_edst_figure(self, save_path):
         edst = Edst.compute_smoothed_edst(self.start_datetime, self.days)
         self.base_plotter.plot_edst(edst)
-        self.base_plotter.customize_edst_plot(self.start_datetime, len(edst))
+        self.base_plotter.customize_edst(self.start_datetime, len(edst))
         self.base_plotter.save_figure(save_path)
 
     def save_euel_figure(self, station, save_path):
         euel = Euel.calculate_euel_for_days(station, self.start_datetime, self.days)
         self.base_plotter.plot_euel(euel)
-        self.base_plotter.customize_euel_plot(station, self.start_datetime, len(euel))
+        self.base_plotter.customize_euel(station, self.start_datetime, len(euel))
         self.base_plotter.save_figure(save_path)
 
     def save_ee_figure(self, station, save_path):
@@ -44,5 +44,8 @@ class MultiDayEeIndexPlotter:
         edst = Edst.compute_smoothed_edst(self.start_datetime, self.days)
         euel = Euel.calculate_euel_for_days(station, self.start_datetime, self.days)
         self.base_plotter.plot_ee(er, edst, euel)
-        self.base_plotter.customize_ee_plot(station, self.start_datetime, len(er))
+        self.base_plotter.customize_ee(station, self.start_datetime, len(er))
         self.base_plotter.save_figure(save_path)
+
+    def show_figure(self):
+        self.base_plotter.show_figure()
