05c272db5699616a5e245299c4c0375bab12efdb
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2025-01-25 18:10:40 +0900

    del base class

--- backend/ee_index/src/plot/ee_index_plotter.py
+++ backend/ee_index/src/plot/ee_index_plotter.py
@@ -1,45 +1,106 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
+import matplotlib.pyplot as plt
 import numpy as np
 from ee_index.src.calc.edst_index import Edst
 from ee_index.src.calc.er_value import Er
 from ee_index.src.calc.euel_index import Euel
-from ee_index.src.plot.base_ee_index_plotter import BaseEeIndexPlotter
+from ee_index.src.constant.time_relation import Min
 
 
 class EeIndexPlotter:
-    """Plotter for daily EE index"""
-
     def __init__(self, start_date: datetime, end_date: datetime):
         self.start_datetime = start_date
         self.days = (end_date - start_date).days + 1
-        self.base_plotter = BaseEeIndexPlotter()
+        self.fig, self.ax = plt.subplots()
 
-    def save_er_figure(self, station, save_path):
+    def plot_er(self, station):
         er = Er(station, self.start_datetime).calc_er_for_days(self.days)
-        self.base_plotter.plot_er(er)
-        self.base_plotter.customize_er(station, self.start_datetime, len(er))
-        self.base_plotter.save_figure(save_path)
+        x_axis, y_axis = np.arange(0, len(er), 1), er
+        self.ax.plot(x_axis, y_axis)
+        self.ax.set_title(
+            f"{self.start_datetime.date()}_{station}_UT",
+            loc="center",
+            fontsize=12,
+            fontweight="bold",
+        )
+        self._set_axis_labels("ER Value(nT)", self.start_datetime, len(er))
 
-    def save_edst_figure(self, save_path):
+    def plot_edst(self):
         edst = Edst.compute_smoothed_edst(self.start_datetime, self.days)
-        self.base_plotter.plot_edst(edst)
-        self.base_plotter.customize_edst(self.start_datetime, len(edst))
-        self.base_plotter.save_figure(save_path)
+        x_axis, y_axis = np.arange(0, len(edst), 1), edst
+        self.ax.plot(x_axis, y_axis)
+        self.ax.set_title(
+            f"{self.start_datetime.date()}_UT",
+            loc="center",
+            fontsize=12,
+            fontweight="bold",
+        )
+        self._set_axis_labels("EDst Value(nT)", self.start_datetime, len(edst))
 
-    def save_euel_figure(self, station, save_path):
+    def plot_euel(self, station):
         euel = Euel.calculate_euel_for_days(station, self.start_datetime, self.days)
-        self.base_plotter.plot_euel(euel)
-        self.base_plotter.customize_euel(station, self.start_datetime, len(euel))
-        self.base_plotter.save_figure(save_path)
+        x_axis, y_axis = np.arange(0, len(euel), 1), euel
+        self.ax.plot(x_axis, y_axis)
+        self.ax.set_title(
+            f"{self.start_datetime.date()}_{station}_UT",
+            loc="center",
+            fontsize=12,
+            fontweight="bold",
+        )
+        self._set_axis_labels("EUEL Value(nT)", self.start_datetime, len(euel))
 
-    def save_ee_figure(self, station, save_path):
+    def plot_ee(self, station):
         er = Er(station, self.start_datetime).calc_er_for_days(self.days)
         edst = Edst.compute_smoothed_edst(self.start_datetime, self.days)
         euel = Euel.calculate_euel_for_days(station, self.start_datetime, self.days)
-        self.base_plotter.plot_ee(er, edst, euel)
-        self.base_plotter.customize_ee(station, self.start_datetime, len(er))
-        self.base_plotter.save_figure(save_path)
+        if len(er) != len(edst) or len(er) != len(euel):
+            raise ValueError("The length of the arrays must be the same")
+        x_axis = np.arange(0, len(er), 1)
+        self.ax.plot(x_axis, er, label="ER", color="black", lw=0.5)
+        self.ax.plot(x_axis, edst, label="EDst", color="green", lw=0.5)
+        self.ax.plot(x_axis, euel, label="EUEL", color="red", lw=0.5)
+        self.ax.set_title(
+            f"{self.start_datetime.date()}_{station}_UT",
+            loc="center",
+            fontsize=12,
+            fontweight="bold",
+        )
+        self._set_axis_labels("EEindex Value(nT)", self.start_datetime, len(er))
 
-    def show_figure(self):
-        self.base_plotter.show_figure()
+    def _set_axis_labels(self, y_label_name, start_datetime, data_length):
+        self.ax.set_ylabel(y_label_name)
+        self.ax.set_xlim(0, data_length)
+        self.ax.set_ylim(-100, 200)
+        x_labels = np.arange(0, data_length, data_length // 8)
+        num_days = data_length // Min.ONE_DAY.const
+        # 表示形式の変更
+        if num_days <= 2:
+            # 時間表示
+            x_tick_labels = [
+                (start_datetime + timedelta(minutes=int(i))).strftime("%H:%M")
+                for i in x_labels
+            ]
+            self.ax.set_xlabel("UT Time")
+        else:
+            # 日付表示
+            x_tick_labels = [
+                (start_datetime + timedelta(days=int(i // Min.ONE_DAY.const))).strftime(
+                    "%m/%d"
+                )
+                for i in x_labels
+            ]
+            self.ax.set_xlabel("UT Date")
+        self.ax.set_xticks(x_labels)
+        self.ax.set_xticklabels(x_tick_labels)
+
+    def show(self):
+        plt.show()
+
+    def save(self, path):
+        """画像保存
+
+        Caution:
+            show後に呼び出すと白い画面が表示される
+        """
+        plt.savefig(path)
