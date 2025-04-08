7931f00f4b2b98762cbe154702a8589355e8706a
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2025-04-08 15:56:28 +0900

    modify constant

--- backend/src/ee_index/constant/eej.py
+++ backend/src/ee_index/constant/eej.py
@@ -1,8 +1,12 @@
 from datetime import time
 
-EEJ_THRESHOLD = 30
+EEJ_THRESHOLD = 10
 
 
 class EejDetectionTime:
     START = time(10, 0)
     END = time(13, 59)
+
+    @classmethod
+    def contains(cls, t: time) -> bool:
+        return cls.START <= t <= cls.END
