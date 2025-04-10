0a00747eec68289321c2cbde6726381167ec8de1
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2025-04-10 18:40:59 +0900

    modify

--- backend/src/service/downloads/zip/remove_files.py
+++ backend/src/service/downloads/zip/remove_files.py
@@ -6,6 +6,6 @@ from src.utils.path import generate_parent_abs_path
 
 
 def remove_files():
-    tmp_dir_p = generate_parent_abs_path(TMP_DIR_PATH)
-    shutil.rmtree(tmp_dir_p)
-    os.mkdir(tmp_dir_p)
+    tmp_dir_path = generate_parent_abs_path(TMP_DIR_PATH)
+    shutil.rmtree(tmp_dir_path)
+    os.mkdir(tmp_dir_path)
