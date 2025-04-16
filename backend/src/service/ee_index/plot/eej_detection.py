5ac0fff4a8abbb901fa4a86f540c0fbd3002d0ff
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2025-04-17 01:12:56 +0900

    def new method: to confirm eej detection

--- backend/src/service/ee_index/plot/eej_detection.py
+++ backend/src/service/ee_index/plot/eej_detection.py
@@ -1,7 +1,8 @@
-from datetime import datetime, timedelta
+from datetime import datetime, time, timedelta
 
 import numpy as np
 from matplotlib import pyplot as plt
+from src.service.ee_index.calc.detect_eej import calc_euel_for_eej_detection
 from src.service.ee_index.calc.euel_index import get_local_euel
 from src.service.ee_index.calc.moving_ave import calc_moving_ave
 from src.service.ee_index.constant.magdas_station import EeIndexStation
@@ -11,6 +12,8 @@ from src.service.ee_index.plot.config import PlotConfig
 
 class EejDetectionPlotter:
     def __init__(self, start_lt: datetime, end_lt: datetime):
+        if start_lt >= end_lt:
+            raise ValueError("start_lt must be less than end_lt.")
         self.start_lt = start_lt
         self.end_lt = end_lt
         PlotConfig.rcparams()
@@ -24,10 +27,25 @@ class EejDetectionPlotter:
         x_axis = np.arange(0, len(moving_avg), 1)
         self.ax.plot(x_axis, moving_avg, label=station.name)
 
-    def plot_pure(self, station: EeIndexStation):
+    def plot_euel_to_detect_eej(self, station: EeIndexStation, color):
+        """EEJを検知するためのプロット
+        注意:
+        EEJの検知は日毎で行うため、start_ltとend_ltは日付の粒度で指定してください
+        """
+        if self.start_lt.time() != time(0, 0) or self.end_lt.time() != time(23, 59):
+            raise ValueError("start_lt must be 00:00 and end_lt must be 23:59.")
+        date_range = [
+            self.start_lt.date() + timedelta(days=i)
+            for i in range((self.end_lt - self.start_lt).days + 1)
+        ]
+        euel = np.hstack([calc_euel_for_eej_detection(station, d) for d in date_range])
+        x_axis = np.arange(0, len(euel), 1)
+        self.ax.plot(x_axis, euel, label=station.name, color=color)
+
+    def plot_pure(self, station: EeIndexStation, color):
         local_euel = get_local_euel(station, self.start_lt, self.end_lt)
         x_axis = np.arange(0, len(local_euel), 1)
-        self.ax.plot(x_axis, local_euel, label=station.name)
+        self.ax.plot(x_axis, local_euel, label=station.name, color=color)
 
     def _set_axis_labels(self):
         data_length = (
@@ -64,11 +82,10 @@ class EejDetectionPlotter:
         plt.savefig(path)
 
 
-start_lt = datetime(2014, 1, 20, 0, 0)
-end_lt = datetime(2014, 1, 20, 23, 59)
-detection = EejDetectionPlotter(start_lt, end_lt)
-detection.plot_local_euel(EeIndexStation.ANC)
-detection.plot_local_euel(EeIndexStation.EUS)
-detection.plot_pure(EeIndexStation.ANC)
-detection.plot_pure(EeIndexStation.EUS)
-detection.show()
+if __name__ == "__main__":
+    start_date = datetime(2014, 1, 1, 0, 0)
+    end_date = datetime(2014, 1, 31, 23, 59)
+    detection = EejDetectionPlotter(start_date, end_date)
+    detection.plot_local_euel(EeIndexStation.ANC)
+    detection.plot_local_euel(EeIndexStation.EUS)
+    detection.show()
