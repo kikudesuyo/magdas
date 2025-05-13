d135daee1ab70f0de33cabc4646037bb13da2046
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2025-05-14 00:38:02 +0900

    refactor: Remove deprecated raw data thresholds and update references in calculations

--- backend/src/constants/ee_index.py
+++ backend/src/constants/ee_index.py
@@ -2,11 +2,16 @@ from enum import Enum
 
 from src.domain.magdas_station import MagdasStation
 
-EEJ_THRESHOLD = 10
+# raw data threshold
+MIN_RAW_H = 0
+MAX_RAW_H = 64000
 
 # ER value threshold
 MAX_ER_VALUE = 300
 MIN_ER_VALUE = -300
+
+# EEJ Value
+EEJ_THRESHOLD = 10
 
 
 class EeIndexStation(MagdasStation, Enum):
