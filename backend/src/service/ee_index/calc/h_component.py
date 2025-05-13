af40084b925ef14e47bd05b24a6d4d99d46f7ee7
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2025-05-13 22:58:41 +0900

    Refactor raw_data_reader import and move read_raw_min_data function to a new file

--- backend/src/service/ee_index/calc/h_component.py
+++ backend/src/service/ee_index/calc/h_component.py
@@ -5,7 +5,7 @@ import numpy as np
 from src.service.ee_index.constant.raw_data import MAX_H, MIN_H
 from src.service.ee_index.constant.time_relation import Min
 from src.service.ee_index.helper.params import CalcParams
-from src.service.ee_index.helper.raw_data_reader import read_raw_min_data
+from src.service.raw_data_reader import read_raw_min_data
 from src.utils.path import generate_parent_abs_path
 
 
