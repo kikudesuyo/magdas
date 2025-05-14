988d16b2c101f7845f747317c2a05e92f02d2ed0
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2025-05-14 14:16:43 +0900

    refactor: Rename variable 'dt' to 'ut' for clarity in date handling

--- backend/src/handler/ee_daily_zip_handler.py
+++ backend/src/handler/ee_daily_zip_handler.py
@@ -36,11 +36,11 @@ def handle_get_daily_ee_index_zip_file(
     request: DownloadEeIndexReq = Depends(DownloadEeIndexReq.from_query),
 ):
     # TODO 現在のファイルははIAGA形式、もし他の形式を実装する場合は、クエリパラメータでフォーマットを指定させる
-    dt = to_datetime(request.date)
+    ut = to_datetime(request.date)
     station = EeIndexStation[request.station_code]
 
-    start_ut = dt.replace(hour=0, minute=0, second=0, microsecond=0)
-    end_ut = dt.replace(hour=23, minute=59, second=59, microsecond=0)
+    start_ut = ut.replace(hour=0, minute=0, second=0, microsecond=0)
+    end_ut = ut.replace(hour=23, minute=59, second=59, microsecond=0)
 
     period = Period(start_ut, end_ut)
     params = StationParams(station, period)
