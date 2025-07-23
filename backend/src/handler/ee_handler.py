bcde60b6c25764d3e9b1f473851d4b12efc21c13
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2025-07-23 17:11:16 +0900

    refactor

--- backend/src/handler/ee_handler.py
+++ backend/src/handler/ee_handler.py
@@ -1,14 +1,11 @@
-from datetime import timedelta
 from typing import Iterable, List
 
 import numpy as np
 from fastapi import Depends, Query
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, Field
-from src.constants.time_relation import TimeUnit
 from src.domain.magdas_station import EeIndexStation
-from src.domain.station_params import Period, StationParam
-from src.service.ee_index.factory_ee import EeFactory
+from src.usecase.ee_by_days import EeIndexByDays
 from src.utils.date import str_to_datetime
 
 
@@ -34,48 +31,21 @@ class EeIndexDateRangeReq(BaseModel):
 def handle_get_ee_by_range(
     req: EeIndexDateRangeReq = Depends(EeIndexDateRangeReq.from_query),
 ):
-    date, station_code, days = (req.start_date, req.station_code, req.days)
-    station = EeIndexStation[station_code]
-    start_ut = str_to_datetime(date)
+    start_ut = str_to_datetime(req.start_date)
+    days = req.days
+    station = EeIndexStation[req.station_code]
 
-    period = Period(start_ut, start_ut + timedelta(days=days))
-    params = StationParam(station=station, period=period)
-
-    factory = EeFactory()
-    er = factory.create_er(params)
-    edst = factory.create_edst(period)
-    euel = factory.create_euel(params)
-
-    # for JSON serialization
-    er_with_none = sanitize_np(er.calc_er())
-    edst_with_none = sanitize_np(edst.calc_edst())
-    euel_with_none = sanitize_np(euel.calc_euel())
-
-    minute_labels = [
-        (start_ut + timedelta(minutes=i)).strftime("%Y-%m-%d %H:%M")
-        for i in range(days * TimeUnit.ONE_DAY.min)
-    ]
-    print(er_with_none)
+    ee_index = EeIndexByDays(start_ut, days, station)
+    er, edst, euel = ee_index.get_ee_index()
+    minute_labels = ee_index.get_minute_labels()
 
     return JSONResponse(
         content={
             "values": {
-                "er": er_with_none,
-                "edst": edst_with_none,
-                "euel": euel_with_none,
+                "er": er,
+                "edst": edst,
+                "euel": euel,
             },
             "minuteLabels": minute_labels,
         }
     )
-
-
-def np_nan_to_none(values: np.ndarray) -> List[float | None]:
-    return [None if np.isnan(x) else x for x in values]
-
-
-def to_float(values: Iterable[float | None]) -> List[float | None]:
-    return [float(x) if x is not None else None for x in values]
-
-
-def sanitize_np(values: np.ndarray) -> List[float | None]:
-    return to_float(np_nan_to_none(values))
