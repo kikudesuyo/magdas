bf30940293b621b019ee2f230c3c3f7458791c1a
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2025-11-22 14:14:17 +0900

    refactor

--- backend/src/service/file_exporter/zip_create.py
+++ backend/src/service/file_exporter/zip_create.py
@@ -1,15 +1,24 @@
 import glob
-import io
 import os
+from io import BytesIO
+from typing import List, Tuple
 from zipfile import ZipFile
 
+from src.model.file import FileModel
 
-def create_zip_buffer(dir_path: str) -> io.BytesIO:
-    zip_buffer = io.BytesIO()
-    with ZipFile(zip_buffer, "w") as zipf:
-        files = glob.glob(os.path.join(dir_path, "**"))
-        for file in files:
-            if os.path.isfile(file):
-                zipf.write(file, arcname=os.path.basename(file))
-    zip_buffer.seek(0)
-    return zip_buffer
+
+class FileService:
+    def create(self, file_bytes: bytes) -> BytesIO:
+        buffer = BytesIO(file_bytes)
+        buffer.seek(0)
+        return buffer
+
+
+class ZipService:
+    def create(self, files: List[FileModel]) -> BytesIO:
+        zip_buffer = BytesIO()
+        with ZipFile(zip_buffer, "w") as zipf:
+            for file in files:
+                zipf.writestr(file.filename, file.content)
+        zip_buffer.seek(0)
+        return zip_buffer
